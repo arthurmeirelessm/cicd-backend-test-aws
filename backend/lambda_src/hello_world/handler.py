93d03eb76dd3532050094cee4f5a2d107530fa20
Arthur Meireles <arthurmsesjdfdn@gmail.com>
2026-01-08 00:49:10 -0300

    change buildspec backend

--- backend/lambda_src/hello_world/handler.py
+++ backend/lambda_src/hello_world/handler.py
@@ -1,5 +1,5 @@
 def handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hello from CI/CD Lambdsddsgdfgsa"
+        'body': 'Hello World - ATUALIZADO VIA CODEBUILD!' 
     }
